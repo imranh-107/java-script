40d3de6f8ae7f7da6d0f0fda35bc89936cb87875
imranh-107 <muhammadimranhosen7@gmail.com>
2025-08-26 01:57:32 +0600

    for loops practis and problen 1 solve

---
+++ forlops.js
@@ -0,0 +1,19 @@
+jfor(let i=1;i<=5;i++){
+    console.log("imran hosen");
+}
+console.log("loops ended");
+
+
+let sum=0;
+for(let i=1;i<=7;i++){
+    sum=sum+i;
+}
+console.log("sum=",sum)
+
+for(let i=0;i<=100;i++){
+    if(i%2!==0){
+         console.log("num=",i);
+    }
+   
+}
+
