d748c0df33bd1865874bda0093e3683569b581d2
imranh-107 <muhammadimranhosen7@gmail.com>
2025-10-28 15:28:18 +0600

    total Time Mobail Use this month

---
+++ function.js/mobailuse time.js
@@ -0,0 +1,8 @@
+function mobailUseTime(perdayPhoneUse){
+    let onemonth=30;
+    let totalTimeMobailUse=onemonth*perdayPhoneUse;
+    console.log(totalTimeMobailUse);
+}
+mobailUseTime(6);
+mobailUseTime(7);
+mobailUseTime(4);
