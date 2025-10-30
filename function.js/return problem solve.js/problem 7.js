e6e12a1f662861a5c8b778e8cd6bffb8787ed552
imranh-107 <muhammadimranhosen7@gmail.com>
2025-10-30 22:02:41 +0600

    practice: add JavaScript function to num<0 then return num*-1

---
+++ function.js/return problem solve.js/problem 7.js
@@ -0,0 +1,12 @@
+function negetive(num){
+    if (num < 0){
+       let total=num*-1;
+        return total;
+    }
+    else{ 
+         return num; 
+    }
+    
+}
+let multiplay=negetive(-5);
+console.log(multiplay);
